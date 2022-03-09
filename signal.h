1d66dd6bce83d7ef86597b53baf8981419329df9
Trần Trung Hiếu <trunghieu05092001@gmail.com>
2022-03-09 08:37:32 +0700

    First commit

---
+++ signal.h
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Signal {
+	private:
+		int n;
+	public:
+		void setN (int n) {
+			this -> n = n;
+		}
+		
+		int getN () {
+			return n;
+		}
+};
