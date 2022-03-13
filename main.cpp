132df2dd37ad578035166abd8216858bb75418d6
Trần Trung Hiếu <trunghieu05092001@gmail.com>
2022-03-13 11:18:55 +0700

    Bieu dien tin hieu bang mang

--- main.cpp
+++ main.cpp
@@ -3,21 +3,10 @@
 using namespace std;
 
 int main() {
-	Signal s1(2,2);
-    cout << "Nhap cac thanh phan cua ma tran: \n";
-    cin >> s1;
-    cout << "Xuat: \n";
-    cout << s1 << endl;
+    DS ds1;
+    ds1.input();
+    ds1.output();
 
-    Signal s2(2,2);
-    cout << "Nhap cac thanh phan cua ma tran: \n";
-    cin >> s2;
-    cout << "Xuat: \n";
-    cout << s2 << endl;
-
-    cout << "Tong: \n";
-    cout << s1 + s2 << endl;
-
-    cout << "Tich: \n";
-    cout << s1 * s2 << endl;
+    system("pause");
+    return 0;
 }
