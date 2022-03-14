412a852608065df36e94590e2cdbd3b822860f41
Trần Trung Hiếu <trunghieu05092001@gmail.com>
2022-03-14 22:46:49 +0700

    Them phep cong va phep nhan 2 tin hieu

--- main.cpp
+++ main.cpp
@@ -7,24 +7,75 @@ int main() {
     ds1.input();
     ds1.output();
 
-    // DS ds2;
-    // ds2.input();
+    DS ds2;
+    ds2.input();
+
+    DS tong = ds1.tong(ds2);
+    tong.output();
+
+    DS tich = ds1.tich(ds2);
+    tich.output();
 
     // ds1.tong(ds2);
     // ds2.output();
 
-    DS ds3 = ds1.downsampling();
-    ds3.output();
 
-    DS ds4 = ds1.upsampling();
-    ds4.output();
+    // int key;
+    // vector<DS> list;
+    // DS ds1, ds2;
+    
+    // while(true) {                
+    //     menu();
 
-    // ds1.shifting();
-    // ds1.output();
+    //     cin >> key;
+    //     switch(key) {
+    //         case 1:
+    //             cout << "\n1. Nhap 2 tin hieu";
+    //             ds1.input();
+    //             ds2.input();
+    //             break;
+    //         case 2:
+    //             cout << "\n2. Tong 2 tin hieu";
+    //             DS tong = ds1.tong(ds2);
+    //             tong.output();
+    //             break;
+    //         case 3:
+    //             cout << "\n3. Tich 2 tin hieu";
+    //             ds1.tich(ds2);
+    //             break;
+    //         case 4:
+    //             cout << "\n4. Nhan tin hieu voi hang so";
+    //             break;
+    //         case 5:
+    //             cout << "\n5. Dich thoi gian";
+    //             ds1.shifting();
+    //             ds1.output();
+    //             break;
+    //         case 6:
+    //             cout << "\n6. Dao thoi gian";
+    //             ds1.reversal();
+    //             ds1.output();
+    //             break;
+    //         case 7:
+    //             cout << "\n7. Tang tan so lay mau (upsampling)";
+    //             DS ds4 = ds1.upsampling();
+    //             ds4.output();
+    //             break;
+    //         case 8:
+    //             cout << "\n8. Giam tan so lay mau (downsampling)";
+    //             DS ds3 = ds1.downsampling();
+    //             ds3.output();
+    //             break;
+    //         case 0:
+    //             cout << "Thoat!\n";
+    //             exit(1);
+    //             break;
+    //         default:
+    //             cout << "Khong ton tai lua chon!\n";
+    //             break;
+    //     }
+    // }
 
-    // ds1.reversal();
-    // ds1.output();
-   
     system("pause");
     return 0;
 }
--- signal.h
+++ signal.h
@@ -11,18 +11,19 @@ class DS {
         // Khoi tao
         DS() {
             size = 0;
+			values.clear();
         }
 
         // Huy
         ~DS() {}
 
         void input() {
-        	cout << "Nhap kich thuoc cua mang:\n";
+        	cout << "\nNhap kich thuoc cua mang tin hieu: ";
         	int s;
         	cin >> s;
         	size = s;
         	
-            cout << "Nhap gia tri cua mang tin hieu:\n";
+            cout << "\nNhap gia tri cua mang tin hieu:\n";
             float x;
             int i = 0;
             do {
@@ -31,7 +32,7 @@ class DS {
             	i++;
 			} while (i < size);
 
-            cout << "Nhap chi so goc toa do:\n";
+            cout << "\nNhap chi so goc toa do: ";
             int origin;
             cin >> origin;
             origin_index = origin;
@@ -39,24 +40,93 @@ class DS {
 
         void output() {
             for(int i = 0; i < values.size(); i++) {
-                cout << values[i] << "; ";
+				if (values[i] != 0) {
+					cout << values[i] << "; ";
+				}                
             }
             cout << "\nChi so goc toa do: " << origin_index << endl; 
         }
 
 		// Tong 2 tin hieu roi rac
-		void tong(const DS& b) {
-
+		DS tong(const DS& b) {
+			DS kq;
+			int n = 0, k = 0, i = 0, o1 = origin_index, o2 = b.origin_index;
+			n = max(o1,o2) + max(size-o1, b.size-o2);
+			int j = 0;
+			do {
+				kq.values.push_back(0);
+				j++;
+			} while (j < n);
+			i = max(o1,o2);
+			kq.origin_index=i;
+			while(o1 - k >= 0 || o2 - k >= 0){
+				if(o1 - k >= 0 && o2 - k >= 0)
+					kq.values[i] = (values[o1 - k] + b.values[o2 - k]);
+				else if(o1 - k >= 0)
+					kq.values[i] = (values[o1 - k]);
+				else
+					kq.values[i] = (b.values[o2 - k]);
+				i--;
+				k++;
+			}
+			i = max(o1,o2);
+			k = 0;
+			while(o1 + k < size || o2 + k < b.size){
+				if(o1 + k < size && o2 + k < b.size)
+					kq.values[i] = (values[o1+k] + b.values[o2+k]);
+				else if(o1+k<size)
+					kq.values[i] = (values[o1+k]);
+				else
+					kq.values[i] = (b.values[o2+k]);
+				i++;
+				k++;
+			}
+			kq.size = i;
+			return kq;
 		}
 
 		// Tich 2 tin hieu roi rac
-		void tich() {
+		DS tich(const DS& b) {
+			DS kq;
+			int n = 0, k = 0, i = 0, o1 = origin_index, o2 = b.origin_index;
+			n = max(o1,o2) + max(size-o1, b.size-o2);
+			int j = 0;
+			do {
+				kq.values.push_back(0);
+				j++;
+			} while (j < n);
+			i = max(o1,o2);
+			kq.origin_index=i;
+			while(o1 - k >= 0 || o2 - k >= 0){
+				if(o1 - k >= 0 && o2 - k >= 0)
+					kq.values[i] = (values[o1 - k] * b.values[o2 - k]);
+				else
+					kq.values[i] = 0;
+				i--;
+				k++;
+			}
+			i = max(o1,o2);
+			k = 0;
+			while(o1 + k < size || o2 + k < b.size){
+				if(o1 + k < size && o2 + k < b.size)
+					kq.values[i] = (values[o1+k] * b.values[o2+k]);
+				else
+					kq.values[i] = 0;
+				i++;
+				k++;
+			}
+			kq.size = i;
+			return kq;
+		}
+
+		// Nhan tin hieu voi 1 hang so
+		void tich_hang() {
 
 		}
 
 		// Dich chuyen thoi gian
 		void shifting() {
-			cout << "Nhap gia tri thoi gian dich chuyen:\n";
+			cout << "\nNhap gia tri thoi gian dich chuyen: ";
 			int m;
 			cin >> m;
 
@@ -77,7 +147,7 @@ class DS {
 		// Gian - tang tan so lay mau
 		DS upsampling() {
 			int l;
-			cout << "Nhap ty so gian:\n";
+			cout << "\nNhap ty so gian: ";
 			cin >> l;
 
 			DS result;
@@ -98,7 +168,7 @@ class DS {
 		// Nen - giam tan so lay mau
 		DS downsampling() {
 			int m;
-			cout << "Nhap ty so nen:\n";
+			cout << "\nNhap ty so nen: ";
 			cin >> m;
 
 			DS result;
@@ -115,4 +185,27 @@ class DS {
 		void tich_chap() {
 
 		}
+
+		// Tuong quan cheo
+		void tuong_quan_cheo() {
+
+		}
 };
+
+void menu() {
+	cout << "CHUONG TRINH XU LY TIN HIEU ROI RAC HUONG DOI TUONG\n";
+	cout << "*************************MENU**************************\n";
+	cout << "**  1. Nhap 2 tin hieu.                              **\n";
+	cout << "**  2. Cong 2 tin hieu.					          **\n";
+	cout << "**  3. Nhan 2 tin hieu.	                          **\n";
+	cout << "**  4. Nhan tin hieu voi 1 hang so.                  **\n";
+	cout << "**  5. Dich thoi gian.								  **\n";
+	cout << "**  6. Dao thoi gian.                   			  **\n";
+	cout << "**  7. Tang tan so lay mau (upsampling).             **\n";
+	cout << "**  8. Giam tan so lay mau (downsampling).           **\n";
+	cout << "**  9. Tich chap.                                    **\n";
+	cout << "**  10. Tuong quan cheo giua 2 tin hieu.             **\n";
+	cout << "**  0. Thoat.                                        **\n";
+	cout << "*******************************************************\n";
+	cout << "Nhap tuy chon: ";
+}
